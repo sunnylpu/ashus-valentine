9b0164ad00c0122a389fcd746082c3863dfc8b2c
sunnylpu <sunnytyagi2004@gmail.com>
2026-02-11 15:32:40 +0530

    Fix build error: Initialize useRef in StarryBackground

--- src/components/background/StarryBackground.tsx
+++ src/components/background/StarryBackground.tsx
@@ -6,7 +6,7 @@ import { Points, PointMaterial } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 function StarField(props: any) {
-    const ref = useRef<any>();
+    const ref = useRef<any>(null);
     const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }));
 
     useFrame((state, delta) => {
